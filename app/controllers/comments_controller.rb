3472ea330e4af0f63420e70ca4a6b060879814f4
yuya024 <yuyaaanissy@gmail.com>
2023-08-31 17:01:32 +0900

    ツイート詳細ページの実装

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+class CommentsController < ApplicationController
+  def index
+    redirect_to tweet_path(params[:tweet_id])
+  end
+
+  def create
+    @comment = current_user.comments.new(comment_params)
+    if params[:comment][:content].empty? && params[:comment][:image].blank?
+      @error_messages = ['文章か写真を投稿してください']
+      set_tweet_detail
+      render 'tweets/show', status: :unprocessable_entity
+    elsif (created_comment = Comment.create_comment(user: current_user, params: comment_params,
+                                                    tweet_id: params[:tweet_id])).is_a?(Comment)
+      flash[:notice] = 'コメントを返信しました'
+      redirect_to tweet_path(params[:tweet_id])
+    else
+      @error_messages = created_comment
+      set_tweet_detail
+      render 'tweets/show', status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content, :image, :tweet_id)
+  end
+
+  def set_tweet_detail
+    @tweet = Tweet.includes(:user, :favorites, :retweets, :comments).find(params[:tweet_id])
+    @comments = Comment.includes(:user).where(tweet_id: params[:tweet_id]).recent.page(params[:page])
+  end
+end
